f723351f11f78089b00400bad1f81f0bd46b12f6
Daniel Solis <daniel.solis5200@gmail.com>
2023-09-10 22:31:36 -0600

    Modifique el z index del boton que desliza hacia arriba ya que quedaba sobre el header

--- src/components/ButtonTop.jsx
+++ src/components/ButtonTop.jsx
@@ -4,7 +4,7 @@ import { AiOutlineArrowUp } from "react-icons/ai";
 const ButtonTop = ({ estamosTop }) => {
   return (
     <button
-      className={`bg-[#000000] hover:opacity-80 duration-200 flex justify-center items-center fixed z-50 right-5 bottom-24 md:bottom-5 p-2 ${estamosTop && 'hidden'}`}
+      className={`bg-[#000000] hover:opacity-80 duration-200 flex justify-center items-center fixed z-30 right-5 bottom-24 md:bottom-5 p-2 ${estamosTop && 'hidden'}`}
       onClick={() => scroll.scrollToTop()}
     >
       <AiOutlineArrowUp className="text-white text-2xl" />
